e48bfa7e3f9de177307a1920e6dac52d7d4264e9
Marioliv0 <marielleoliveira@alunos.utfpr.edu.br>
2025-04-09 08:56:40 -0300

    Add files via upload

--- 0904.c
+++ 0904.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
-int main () {
-float n1, n2, n3, media, frequência;
+int main ()
+{
+    float n1, n2, n3, media, frequência;
 
-printf("Digite as três notas separadas por espaço: ");
-scanf("%f %f %f", &n1, &n2, &n3);
+    printf("Digite as três notas separadas por espaço: ");
+    scanf("%f %f %f", &n1, &n2, &n3);
 
-printf("Digite a frequência do aluno (em%%): ");
-scanf("%f", &frequência);
+    printf("Digite a frequência do aluno (em%%): ");
+    scanf("%f", &frequência);
 
-media = ((n1+n2+n3)/3.0);
+    media = ((n1+n2+n3)/3.0);
 
-printf("/nmedia: %.2f/n", media);
-printf("frequência: %.1f%%/n", frequência);
+    printf("/nmedia: %.2f/n", media);
+    printf("frequência: %.1f%%/n", frequência);
 
-if (media >= 8.0 && frequência >= 75.0) {
-printf("Aprovado com Distinção. ");
-} else if (media >=6 <8 && frequência >= 75.0) {
-printf("Aprovado Direto. ");
-} else if (media >= 4 && frequência >= 75.0) {
-printf("Vai para a final. ");
-}else if (media< 4 && frequência < 75.0) {
-printf("Reprovado Direto. ");
-}
+    if (media >= 8.0 && frequência >= 75.0)
+    {
+        printf("Aprovado com Distinção. ");
+    }
+    else if (media >=6 <8 && frequência >= 75.0)
+    {
+        printf("Aprovado Direto. ");
+    }
+    else if (media >= 4 && frequência >= 75.0)
+    {
+        printf("Vai para a final. ");
+    }
+    else if (media< 4 && frequência < 75.0)
+    {
+        printf("Reprovado Direto. ");
+    }
 
-return 0;
+    return 0;
 }
 
